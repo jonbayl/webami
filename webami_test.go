75199508d25c635150db80ee588e7728ee8d7185
Jon B <jon.bayliss2@btinternet.com>
2021-04-10 20:48:05 +0100

    Test improvements (#8)
    * tests: replaced help() test with a Go example
    
    * tests: Added Go example for printIp(). Removed os.exit from func.
    
    * fix: added else to if to address panic with no args supllied.
    
    * tests: added tests for main function.

--- webami_test.go
+++ webami_test.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 	"os"
-	"os/exec"
 	"reflect"
+	"regexp"
 	"testing"
 )
 
-func TestHelp(t *testing.T) {
-	if os.Getenv("TEST_HELP") == "1" {
-		help()
-		return
-	}
-	cmd := exec.Command(os.Args[0], "-test.run=TestHelp")
-	fmt.Printf("%s", os.Args[0])
-	cmd.Env = append(os.Environ(), "TEST_HELP=1")
-	err := cmd.Run()
+// func TestHelp(t *testing.T) {
+// 	if os.Getenv("TEST_HELP") == "1" {
+// 		help()
+// 		return
+// 	}
+// 	cmd := exec.Command(os.Args[0], "-test.run=TestHelp")
+// 	fmt.Printf("%s", os.Args[0])
+// 	cmd.Env = append(os.Environ(), "TEST_HELP=1")
+// 	err := cmd.Run()
 
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		t.Errorf("Help returned %v when exit status 0 was expected.", err)
-	}
+// 	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+// 		t.Errorf("Help returned %v when exit status 0 was expected.", err)
+// 	}
+// 	help()
+// }
+
+func Example_help() {
 	help()
+
+	// Output:
+	// webami - A whoami for the internet.
+	// Easily retrieve your public IP address from the command-line. webami utilises Ipify to retrieve your public IP address and return it within the command-line.
+	//
+	// Usage: simply run the webami executable to retrieve your public IP address.
+	//
+	// Other arguments:
+	// version: prints the current version of webami.
+	// use: specify the IP retrieveal service to use. Takes one argument which must be a valid URL, all other arguments are ignored. Example: webami use https://api.ipify.org
 }
+
+func Example_printIp() {
+	printIp([]byte("10.10.10.10"))
+
+	// Output: 10.10.10.10
+}
+
 func TestValidateIp(t *testing.T) {
 	validIp := []byte("10.10.10.10")
 	invalidIp := []byte("this is invalid input")
@@ -64,5 +85,62 @@ func TestGetPublicIp(t *testing.T) {
 	err := validateIp(ip)
 	if err != nil {
 		t.Errorf("getPublicIp returned %s, which is not valid input.", ip)
+	}
+}
+
+func TestMainNoargs(t *testing.T) {
+	os.Args = []string{"webami"}
+
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	match, _ := regexp.Match("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)", out)
+	if match != true {
+		t.Errorf("Expected IP address(x.x.x.x), got: %s", out)
+	}
+
+}
+
+func TestMainVersion(t *testing.T) {
+	os.Args = []string{"webami", "version"}
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	match, _ := regexp.Match("^webami version: v[1-9]\\.[1-9]", out)
+	if match != true {
+		t.Errorf("Expected: webami version: x.x, got: %s", out)
+	}
+}
+
+func TestMainUse(t *testing.T) {
+	os.Args = []string{"webami", "use", "https://api.ipify.org"}
+
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	match, _ := regexp.Match("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)", out)
+	if match != true {
+		t.Errorf("Expected IP address(x.x.x.x), got: %s", out)
 	}
 }
